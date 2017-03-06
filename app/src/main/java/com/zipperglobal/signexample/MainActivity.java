0ddc11a5b14dd35aa6f76e6efcdf1c83f6072fca
Tom Swindell <t.swindell@rubyx.co.uk>
2017-03-06 12:21:34 +0000

    Initial commit
    Signed-off-by: Tom Swindell <t.swindell@rubyx.co.uk>

---
+++ app/src/main/java/com/zipperglobal/signexample/MainActivity.java
@@ -0,0 +1,80 @@
+package com.zipperglobal.signexample;
+
+import android.os.Bundle;
+import android.support.design.widget.FloatingActionButton;
+import android.support.design.widget.Snackbar;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.view.View;
+import android.view.Menu;
+import android.view.MenuItem;
+
+import org.ethereum.crypto.*;
+import org.spongycastle.util.encoders.Hex;
+
+public class MainActivity extends AppCompatActivity {
+    protected ECKey m_key;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+
+        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
+        fab.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
+                        .setAction("Action", null).show();
+            }
+        });
+
+        byte[] message = "SOME MESSAGE TO SIGN".getBytes();
+        byte[] hash = HashUtil.sha3(message);
+
+        m_key = new ECKey();
+        byte[] addr = m_key.getAddress();
+        byte[] priv = m_key.getPrivKeyBytes();
+
+        String addrStr = Hex.toHexString(addr);
+        String privStr = Hex.toHexString(priv);
+
+        System.out.println("ADDRESS: " + addrStr);
+        System.out.println("PRIVATE KEY: " + privStr);
+
+        ECKey.ECDSASignature sig = m_key.sign(hash);
+        System.out.println(" SIGNATURE: " + sig.toHex());
+
+        if(!m_key.verify(hash, sig))
+        {
+            System.out.println("MESSAGE VALIDATION FAILED!");
+            return;
+        }
+
+        System.out.println("MESSAGE VALIDATED SUCCESSFULLY!");
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_main, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.action_settings) {
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+}
